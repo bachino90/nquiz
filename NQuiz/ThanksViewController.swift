9cdbe39e74808de6597ae70fb1f35f5b869c81b0
bachino90 <emilianobivachi@gmail.com>
2016-04-16 17:46:27 -0300

    Show answer

--- NQuiz/ThanksViewController.swift
+++ NQuiz/ThanksViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class ThanksViewController: UIViewController {
     
+    private let answer: String
+    @IBOutlet weak var answerLabel: UILabel!
+    
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
     
     init(answer: String) {
+        self.answer = answer
         super.init(nibName: "ThanksViewController", bundle: nil)
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        answerLabel.text = answer
     }
     
     override func viewDidAppear(animated: Bool) {
